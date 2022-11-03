9afa0a1097875d39900f2876fabbf12750d2ff1e
Ayodele Alakija <alakijaayo@gmail.com>
2022-11-03 08:38:56 +0000

    Adding Java folders

---
+++ src/main/java/UnitedBE/UnitedBE/UnitedBeApplication.java
@@ -0,0 +1,13 @@
+package UnitedBE.UnitedBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitedBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UnitedBE/UnitedBE/UnitedBeApplicationTests.java
@@ -0,0 +1,13 @@
+package UnitedBE.UnitedBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitedBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
